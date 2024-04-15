c47a614c7fa60c3ac54caedb6a614ee7ed8973e8
Gage <77414756+Royuin@users.noreply.github.com>
2024-04-14 19:50:01 -0500

    Added mongoose as a dependency and made app use it

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const passport = require('passport-local');
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
