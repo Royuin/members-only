e350d27b11a9f0bf80a00d8f90653200b3faa1e5
Gage <77414756+Royuin@users.noreply.github.com>
2024-04-20 17:41:12 -0500

    Added a model schema for users

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  username: { type: String, required: true },
+  hash: { type: Sting, required: true },
+  member : { type: Boolean, required: true },
+});
+  
