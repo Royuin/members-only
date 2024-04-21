c7f5e0600878812db28954947a1105e0c44ea446
Gage <77414756+Royuin@users.noreply.github.com>
2024-04-21 10:52:24 -0500

    Made function for getting sign up form use a method from userController to seperate functionality

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
+const expressAsyncHandler = require('express-async-handler');
 var router = express.Router();
+const userController = require('../controllers/userController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Members Only' });
 });
 
-router.get('/sign-up', function(req, res, next) {
-  res.render('sign_up', {
-    title: 'Sign Up',
-    });
-});
+router.get('/sign-up', userController.signUpGet); 
+
+// router.post('/sign-up', expressAsyncHandler( async (req, res, next) {
+//
+// }));
 
 module.exports = router;
