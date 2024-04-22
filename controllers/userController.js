2bfe0cefbb878f53b3bda0f9168fee5db98e0525
Gage <77414756+Royuin@users.noreply.github.com>
2024-04-21 20:29:16 -0500

    Made signUpPost method to validate and sanitize form data and create a new  user with a hashed password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
-const { validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/user');
+const asyncHandler = require('express-async-handler');
+const genPassword = require('../lib/passwordUtils').genPassword;
 
 exports.signUpGet = (req, res, next) => { 
   res.render('sign_up', {
@@ -7,3 +9,25 @@ exports.signUpGet = (req, res, next) => {
   });
 };
 
+exports.signUpPost =  [
+  body('firstName', 'First name must not be empty.').trim().escape(),
+  body('lastName', 'Last name must not be empty.').trim().escape(),
+  body('username', 'Username must not be empty and be at least 4 characters long.').trim().isLength({min: 4}).escape(),
+  body('password', 'Password must not be empty and be at least 4 characters long.').trim().isLength({min: 4}).escape(),
+  
+  asyncHandler( async (req, res, next) => {
+   const errors = validationResult(req);
+   const hash = await genPassword(req.body.password);
+  
+   const newUser = new User({
+     firstName: req.body.firstName,
+     lastName: req.body.lastName,
+     username: req.body.username,
+     hash: hash,
+     member: false,
+     });
+
+    await newUser.save();
+    res.redirect('/sign-up');
+  }),
+];
