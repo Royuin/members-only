b7cf7c607205953d5d7af034bec0f21cec43158c
Gage <77414756+Royuin@users.noreply.github.com>
2024-04-26 21:42:24 -0500

    Made createMessageGet function check if user is logged in before displaying page. If user is not logged in then redirect user to a page that informs them with links to login or sign up

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,7 +3,13 @@ const Message = require('../models/message');
 const asyncHandler = require('express-async-handler');
 
 exports.createMessageGet = (req, res, next) => {
-  res.render('create_message_form', {
-    title: 'Create Message',
-  });
+  if (req.isAuthenticated()) {
+    res.render('create_message_form', {
+      title: 'Create Message',
+    });
+  } else {
+    res.render('not_logged_in', {
+    title: 'Not Logged In',
+    });
+  };
 };
