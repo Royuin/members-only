216fedad2253a71dc897973f47aa82bec47f7920
Gage <77414756+Royuin@users.noreply.github.com>
2024-04-27 23:02:14 -0500

    Added new value to model for reference to author of message

--- models/message.js
+++ models/message.js
@@ -5,6 +5,7 @@ const MessageSchema = new Schema({
   title: { type: String, maxLength: 20, required: true },
   message: { type: String, maxlength: 100, required: true },
   dateUploaded: { type: Date, default: Date.now }, 
+  author: { type: Schema.ObjectId, ref: 'User' }
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
