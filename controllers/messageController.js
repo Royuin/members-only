9280eb82720428ebe38cd94ac94508a9d4f974bd
Gage <77414756+Royuin@users.noreply.github.com>
2024-04-27 23:11:02 -0500

    Made post request function for creating a message validate submitted info and create message or re render on error

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -9,7 +9,27 @@ exports.createMessageGet = (req, res, next) => {
     });
   } else {
     res.render('not_logged_in', {
-    title: 'Not Logged In',
+      title: 'Not Logged In',
     });
   };
 };
+
+exports.createMessagePost = [
+  body('title', 'Title must not be empty.').notEmpty().trim().isLength({ max: 20 }).escape(),
+  body('message', 'Message must not be empty.').notEmpty().trim().isLength({ max: 100 }).escape(),
+
+  asyncHandler ( async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+      res.render('create_message_form', {
+        title: 'Create Message',
+        errors: errors,
+      });
+    } else {
+      const newMessage = new Message({ title: req.body.title, message: req.body.message, author: req.user });
+      await newMessage.save();
+      res.redirect('/');
+    }
+  }),
+];
