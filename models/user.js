67407c6bab3348c5b38a1e12487a3e22351c8961
Gage <77414756+Royuin@users.noreply.github.com>
2024-04-28 19:32:44 -0500

    Added a admin property to model

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   hash: { type : String, required: true },
   member: { type: Boolean, required: true },
+  admin: { type: Boolean, required: true },
 });
   
 module.exports = mongoose.model('User', UserSchema);
